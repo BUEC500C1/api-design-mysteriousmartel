c722d09ca3e40c7f2f97d1337f04ed538e0694cf
Jennifer Campbell <48844772+mysteriousmartel@users.noreply.github.com>
2020-02-04 23:56:02 -0500

    Add files via upload

--- tweetImgSummary.py
+++ tweetImgSummary.py
@@ -1,3 +1,5 @@
+# Copyright 2020 by Jennifer Campbell
+
 import tweepy
 import urllib.request
 import io
@@ -6,7 +8,14 @@ import os
 from google.cloud import vision
 from google.cloud.vision import types
 
+''' This API is built to filter through N number of tweets
+with a search parameter, pull out only tweets with images, then 
+returns the strongest associated image label, as well as the
+tweet associated with it'''
+
 def tweetCall(searchParam, tweetNum):
+
+  tweetDir = os.getcwd()
 
   consumer_key = ''
   consumer_secret = ''
@@ -21,6 +30,7 @@ def tweetCall(searchParam, tweetNum):
 
   tweetInd = []
 
+
   for tweet in tweetList:
     try:
       if tweet.entities['media'][0]['type'] == 'photo':
@@ -31,12 +41,44 @@ def tweetCall(searchParam, tweetNum):
     except BaseException:
       pass
 
-
+  for i in range(len(tweetInd)):
+    link = str(tweetInd[i])
+    imgIndex = "%04d" % i
+    urllib.request.urlretrieve(link, f"jpg{imgIndex}.jpg")
 '''    if 'text' in tweet._json:
       tweetInd.append(tweet._json['text'])
 
       with open("feedList.txt", 'a+') as f:
         f.write(tweet._json['text'] + '\n\n')'''
 
+def imgSentiment():
+
+  imgName = list(os.popen('ls'))
+  labelsList = []
+
+  for img in imgName:
+
+    if '.jpg' in img:
+      images = img.strip()
+      client = vision.ImageAnnotatorClient()
+
+      fileName = os.path.join(os.path.dirname(__file__), images)
+
+      with io.open(fileName, 'rb') as file:
+        content = file.read()
+
+      image = types.Image(content=content)
+
+      response = client.label_detection(image=image)
+      labels = response.label_annotations
+      labelsList.append(labels[0].description)
+
+  return labelsList
+
+def tweetSummary(searchParam, tweetNum):
+  tweetCall(searchParam, tweetNum)
+  imgsentiment()
+
 if __name__ == '__main__':
-  tweetCall('dog', 50)
+  tweetCall('puppy', 50)
+  print(imgSentiment())
