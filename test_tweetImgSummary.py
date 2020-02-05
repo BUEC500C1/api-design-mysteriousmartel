19eaf54dfbfa638efde729e0d84fb4cdec07d951
Jennifer Campbell <48844772+mysteriousmartel@users.noreply.github.com>
2020-02-05 16:23:22 -0500

    Add files via upload

--- test_tweetImgSummary.py
+++ test_tweetImgSummary.py
@@ -3,5 +3,5 @@ import tweetImgSummary
 def test_tweetArrangement():
     puppy = tweetImgSummary.tweetSummary('puppy', 100)
     puppyKeys = list(puppy)
-    labels = len(imgSentiment())
+    labels = len(tweetImgSummary.imgSentiment())
     assert len(puppy) == labels
